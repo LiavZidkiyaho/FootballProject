3d31989c2f6fd20bc4c3f9633b939a5a5358588d
Liav Zidkiyaho <liavz2909@gmail.com>
2025-01-19 18:11:57 +0200

    Created vs project

---
+++ FootballProject/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FootballProject
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
